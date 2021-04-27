35123fac13c684cb58842c3983afb45f8884573e
luizsp7m <62671884+luizsp7m@users.noreply.github.com>
2021-04-27 20:35:50 -0300

    Modal started

--- 05. Crowdfunding Product Page/script.js
+++ 05. Crowdfunding Product Page/script.js
@@ -4,3 +4,13 @@ const menuToggle = () => {
   navbarNav.classList.toggle('toggled');
   menuToggleIcon.classList.toggle('toggled');
 }
+
+const showModal = () => {
+  const modal = document.querySelector('#modal');
+  modal.style.display = "flex";
+}
+
+const closeModal = () => {
+  const modal = document.querySelector('#modal');
+  modal.style.display = "none";
+}
