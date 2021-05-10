037d42465b70f5a9acb4e0061bfe3386f2d1cfc6
luizsp7m <62671884+luizsp7m@users.noreply.github.com>
2021-05-10 15:04:19 -0300

    Finished project

--- 05. Crowdfunding Product Page/script.js
+++ 05. Crowdfunding Product Page/script.js
@@ -14,3 +14,34 @@ const closeModal = () => {
   const modal = document.querySelector('#modal');
   modal.style.display = "none";
 }
+
+
+let count = 0;
+let last = undefined;
+
+const selected = (el) => {
+  if(count === 0) {
+    document.querySelectorAll('.card-booking')[el].style.display = 'flex';
+    document.querySelectorAll('.card-input .checkbox')[el].style.background = 'hsl(176, 50%, 47%)';
+    count++;
+    last = el;
+  } else {
+    document.querySelectorAll('.card-booking')[last].style.display = 'none';
+    document.querySelectorAll('.card-input .checkbox')[last].style.background = 'none';
+    document.querySelectorAll('.card-input .checkbox')[el].style.background = 'hsl(176, 50%, 47%)';
+    document.querySelectorAll('.card-booking')[el].style.display = 'flex';
+    last = el;
+  }
+}
+
+const showSuccess = () => {
+  closeModal();
+
+  const success = document.querySelector('#success');
+  success.style.display = 'flex';
+}
+
+const closeSuccess = () => {
+  const success = document.querySelector('#success');
+  success.style.display = 'none';
+}
