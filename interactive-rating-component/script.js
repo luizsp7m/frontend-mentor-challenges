81503bd15a44e5856acd375782e1939012add29e
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-03-29 22:21:21 -0300

    Desafio concluído

--- interactive-rating-component/script.js
+++ interactive-rating-component/script.js
@@ -1,16 +1,31 @@
 const welcome = document.querySelector("#welcome");
 const thanks = document.querySelector("#thanks");
 const submit = document.querySelector("#submit");
+const note = document.querySelector("#note-selected");
+const buttons = document.querySelectorAll(".card-rating button");
 
-// let note = undefined;
+let note_selected = undefined;
 
 thanks.style.display = "none";
 
 function redirect() {
-  // if (note) {
+  if (note_selected >= 0) {
     welcome.style.display = "none";
     thanks.style.display = "flex";
-  // }
+    note.textContent = `${note_selected + 1}`;
+  }
 }
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    if (note_selected >= 0) {
+      buttons[note_selected].classList.remove("selected");
+    }
+
+    note_selected = index;
+
+    buttons[index].classList.add("selected");
+  });
+});
 
 submit.addEventListener("click", redirect);
